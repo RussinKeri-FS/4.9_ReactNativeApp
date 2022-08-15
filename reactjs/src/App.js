abdea02e1d1f235a49545b5f9a2dd334abfaa084
Keri Russin <karussin@student.fullsail.edu>
2022-08-15 17:56:45 -0400

    added full crud

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -1,49 +1,18 @@
-import React, { useState, useEffect } from "react";
-import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
+import Home from './pages/Home'
+import Dashboard from './pages/Dashboard'
+import Book from './pages/Book'
 
 function App() {
-  const [books, setBooks] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
-  const API_BASE =
-    process.env.NODE_ENV === "development"
-    ? `http://localhost:8000/api/v1`
-      : process.env.REACT_APP_BASE_URL;
-
-  let ignore = false;
-  useEffect(() => {
-    if (!ignore) {
-      getBooks();
-    }
-    return () => {
-      ignore = true;
-    };
-  }, []);
-
-  const getBooks = async () => {
-    setLoading(true);
-    try {
-      await fetch(`${API_BASE}/books`)
-        .then((res) => res.json())
-        .then((data) => {
-          console.log({ data });
-          setBooks(data);
-        });
-    } catch (error) {
-      setError(error.message || `Unexpected Error`);
-    } finally {
-      setLoading(false);
-    }
-  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Books:</h1>
-        <ul>
-          <li>Books</li>
-        </ul>
-      </header>
-    </div>
+   <Router>
+      <Routes>
+        <Route path="/books/:id" exact element={<Book />} />
+        <Route path="/dashboard" exact element={<Dashboard />} />
+        <Route path="/" exact element={<Home />} />
+      </Routes>
+   </Router>
   );
 }
 
